75aa69dd085a900a927995fa1ffda57f64dfe573
Pramodh Pallapothu <pramodh@zededa.com>
2023-06-08 16:43:54 -0700

    Addressed review comments
    Signed-off-by: Pramodh Pallapothu <pramodh@zededa.com>
    (cherry picked from commit 70f0d93a55cb1ddc0f09458320db487ccfb76db8)

--- pkg/kube/cluster-init.sh
+++ pkg/kube/cluster-init.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/sh
 #
 # Copyright (c) 2023 Zededa, Inc.
 # SPDX-License-Identifier: Apache-2.0
@@ -8,8 +8,7 @@ KUBEVIRT_VERSION=v0.59.0
 LONGHORN_VERSION=v1.4.2
 CDI_VERSION=v1.56.0
 
-INSTALL_LOG=/var/log/install.log
-date >> $INSTALL_LOG
+INSTALL_LOG=/var/lib/install.log
 
 logmsg() {
    local MSG
@@ -24,21 +23,20 @@ setup_cgroup () {
 }
 
 
-                       
 check_network_connection () {
  while true; do
 
-   IP="8.8.8.8"
-   if ping -c 5  $IP > /dev/null 2>&1; then
-        logmsg "Network available, ping to $IP was successful."
+   ret=$(curl -o /dev/null -w "%{http_code}" -s "https://get.k3s.io")
+   if [ "$ret" -eq 200 ]; then
+        logmsg "Network is ready."
         break;
    else
-        logmsg "Ping to $IP failed."
+        logmsg "Network is not yet ready"
    fi
 
    sleep 5
  done
-}                                                               
+}
 
 wait_for_default_route() {
   while read -r iface dest gw flags refcnt use metric mask mtu window irtt; do
@@ -55,48 +53,52 @@ wait_for_default_route() {
 
 #Prereqs
 setup_prereqs () {
-	modprobe tun                                                        
-	modprobe vhost_net    
-	modprobe fuse         
-	modprobe iscsi_tcp
-	#Needed for iscsi tools
-	mkdir -p /run/lock
-	/usr/sbin/iscsid start
-	mount --make-rshared /
-	setup_cgroup                                                   
-
-	#Check network and default routes are up
-	wait_for_default_route
-	check_network_connection
+        modprobe tun
+        modprobe vhost_net
+        modprobe fuse
+        modprobe iscsi_tcp
+        #Needed for iscsi tools
+        mkdir -p /run/lock
+        /usr/sbin/iscsid start
+        mount --make-rshared /
+        setup_cgroup
+        #Check network and default routes are up
+        wait_for_default_route
+        check_network_connection
 }
 
-#Make sure all prereqs are set
-setup_prereqs
-                                                              
-
-#Is this ZFS block device ?
+# NOTE: We only support zfs storage in production systems because data is persisted on zvol.
+# If ZFS is not available we still go ahead and provide the service but the data is lost on reboot
+# because /var/lib will be on overlayfs. The only reason to allow that is to provide a quick debugging env for developers.
 if [ -b /dev/zvol/persist/clustered-storage ]; then
         mount /dev/zvol/persist/clustered-storage /var/lib  ## This is where we persist the cluster components (k3s containers)
+        logmsg "Using ZFS persistent storage"
+else
+        logmsg "WARNING: Using overlayfs non-persistent storage"
 fi
 
-HOSTNAME=`/bin/hostname`
+#Make sure all prereqs are set after /var/lib is mounted to get logging info
+setup_prereqs
+
+date >> $INSTALL_LOG
+HOSTNAME=$(/bin/hostname)
 #Forever loop every 15 secs
 while true;
 do
 if [ ! -f /var/lib/all_components_initialized ]; then
-	if [ ! -f /var/lib/k3s_initialized ]; then
-		#/var/lib is where all kubernetes components get installed.
-		logmsg "Installing K3S version $K3S_VERSION"
-		mkdir -p /var/lib/k3s/bin
-		/usr/bin/curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${K3S_VERSION} INSTALL_K3S_SKIP_ENABLE=true INSTALL_K3S_BIN_DIR=/var/lib/k3s/bin sh -
-		ln -s /var/lib/k3s/bin/* /usr/bin
-		logmsg "Initializing K3S version $K3S_VERSION"
-		nohup /usr/bin/k3s server --config /etc/rancher/k3s/config.yaml &
-		#wait until k3s is ready
-		logmsg "Looping until k3s is ready"
-		until kubectl get node | grep $HOSTNAME | awk '{print $2}' | grep 'Ready'; do sleep 5; done
-		logmsg "k3s is ready on this node"
-		# Default location where clients will look for config
+        if [ ! -f /var/lib/k3s_initialized ]; then
+                #/var/lib is where all kubernetes components get installed.
+                logmsg "Installing K3S version $K3S_VERSION"
+                mkdir -p /var/lib/k3s/bin
+                /usr/bin/curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=${K3S_VERSION} INSTALL_K3S_SKIP_ENABLE=true INSTALL_K3S_BIN_DIR=/var/lib/k3s/bin sh -
+                ln -s /var/lib/k3s/bin/* /usr/bin
+                logmsg "Initializing K3S version $K3S_VERSION"
+                nohup /usr/bin/k3s server --config /etc/rancher/k3s/config.yaml &
+                #wait until k3s is ready
+                logmsg "Looping until k3s is ready"
+                until kubectl get node | grep "$HOSTNAME" | awk '{print $2}' | grep 'Ready'; do sleep 5; done
+                logmsg "k3s is ready on this node"
+                # Default location where clients will look for config
                 ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
                 touch /var/lib/k3s_initialized
         fi
@@ -109,7 +111,7 @@ if [ ! -f /var/lib/all_components_initialized ]; then
 
                 #CDI (containerzed data importer) is need to convert qcow2/raw formats to Persistent Volumes and Data volumes
                 #Since CDI goes with kubevirt we install with that.
-                logmsg "Installing CDI version $CDI_VERSION" 
+                logmsg "Installing CDI version $CDI_VERSION"
                 kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator.yaml
                 kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-cr.yaml
                 touch /var/lib/kubevirt_initialized
@@ -119,27 +121,26 @@ if [ ! -f /var/lib/all_components_initialized ]; then
                 logmsg "Installing longhorn version ${LONGHORN_VERSION}"
                 kubectl apply -f  https://raw.githubusercontent.com/longhorn/longhorn/${LONGHORN_VERSION}/deploy/longhorn.yaml
                 touch /var/lib/longhorn_initialized
-	fi
+        fi
 
-	if [ -f /var/lib/k3s_initialized -a -f /var/lib/kubevirt_initialized -a -f /var/lib/longhorn_initialized ]; then
-		logmsg "All components initialized"
-		touch /var/lib/all_components_initialized
-	fi
+        if [ -f /var/lib/k3s_initialized ] && [ -f /var/lib/kubevirt_initialized ] && [ -f /var/lib/longhorn_initialized ]; then
+                logmsg "All components initialized"
+                touch /var/lib/all_components_initialized
+        fi
 else
-	ps -ef | grep "k3s server" | grep -v "grep" >> $INSTALL_LOG
-	if [ $? -eq 0 ]; then
-		logmsg "k3s is alive "
-	else
-		## Must be after reboot
-		ln -s /var/lib/k3s/bin/* /usr/bin
-		logmsg "Starting k3s server after reboot"
-		nohup /usr/bin/k3s server --config /etc/rancher/k3s/config.yaml &
-		logmsg "Looping until k3s is ready"
-		until kubectl get node | grep $HOSTNAME | awk '{print $2}' | grep 'Ready'; do sleep 5; done
-		logmsg "k3s is ready on this node"
-		# Default location where clients will look for config
-		ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
-	fi
+        if pgrep k3s >> $INSTALL_LOG 2>&1; then
+                logmsg "k3s is alive "
+        else
+                ## Must be after reboot
+                ln -s /var/lib/k3s/bin/* /usr/bin
+                logmsg "Starting k3s server after reboot"
+                nohup /usr/bin/k3s server --config /etc/rancher/k3s/config.yaml &
+                logmsg "Looping until k3s is ready"
+                until kubectl get node | grep "$HOSTNAME" | awk '{print $2}' | grep 'Ready'; do sleep 5; done
+                logmsg "k3s is ready on this node"
+                # Default location where clients will look for config
+                ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/config
+        fi
 fi
-	sleep 15
+        sleep 15
 done
