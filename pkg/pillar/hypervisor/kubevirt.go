c74c0aa2ec9828793dc6adcb61658207ed460ccd
Pramodh Pallapothu <pramodh@zededa.com>
2025-06-26 10:10:09 -0700

    Handle already exists error
    Signed-off-by: Pramodh Pallapothu <pramodh@zededa.com>

--- pkg/pillar/hypervisor/kubevirt.go
+++ pkg/pillar/hypervisor/kubevirt.go
@@ -542,6 +542,12 @@ func (ctx kubevirtContext) Start(domainName string) error {
 	for {
 		_, err = virtClient.ReplicaSet(kubeapi.EVEKubeNameSpace).Create(repvmi)
 		if err != nil {
+			if errors.IsAlreadyExists(err) {
+				// VMI could have been already started, for example failover from other node.
+				// Its not an error, just proceed.
+				logrus.Infof("Start VMI replicaset failed %v\n", err)
+				break
+			}
 			if strings.Contains(err.Error(), "dial tcp 127.0.0.1:6443") && i <= 0 {
 				logrus.Infof("Start VMI replicaset failed %v\n", err)
 				return err
