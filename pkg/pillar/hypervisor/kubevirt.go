865f9f843e8cb3e644120be21f23134d98c22dab
Pramodh Pallapothu <pramodh@zededa.com>
2025-07-31 11:34:28 -0700

    Add FML app support
    Signed-off-by: Pramodh Pallapothu <pramodh@zededa.com>

--- pkg/pillar/hypervisor/kubevirt.go
+++ pkg/pillar/hypervisor/kubevirt.go
@@ -331,6 +331,10 @@ func (ctx kubevirtContext) CreateReplicaVMIConfig(domainName string, config type
 	vmi.Spec.Networks = nads
 	vmi.Spec.Domain.Devices.Interfaces = intfs
 
+	if config.VirtualizationMode == types.FML {
+		addEFIBootLoader(&vmi.Spec)
+	}
+
 	// First check the diskStatusList and ignore 9P
 	// Set Storage
 	if len(diskStatusList) > 0 {
@@ -1750,6 +1754,20 @@ func addKernelBootContainer(spec *v1.VirtualMachineInstanceSpec, image, kernelAr
 	return spec
 }
 
+func addEFIBootLoader(spec *v1.VirtualMachineInstanceSpec) *v1.VirtualMachineInstanceSpec {
+	if spec.Domain.Firmware == nil {
+		spec.Domain.Firmware = &v1.Firmware{}
+	}
+	// Refer https://pkg.go.dev/kubevirt.io/api/core/v1#EFI
+	spec.Domain.Firmware.Bootloader = &v1.Bootloader{
+		EFI: &v1.EFI{
+			SecureBoot: ptrBool(false),
+			Persistent: ptrBool(false),
+		},
+	}
+	return spec
+}
+
 // PCIReserve reserves a PCI device for Kubevirt
 func (ctx kubevirtContext) PCIReserve(long string) error {
 	return PCIReserveGeneric(long)
@@ -1819,4 +1837,8 @@ func isK3sUnreachable(err error) bool {
 		return true
 	}
 	return false
+}
+
+func ptrBool(b bool) *bool {
+	return &b
 }
